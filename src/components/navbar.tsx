f061c5e2662bd91d5dcee114a0a72b257a9705b4
mundume <nzaih18@gmail.com>
2024-11-05 18:08:44 +0300

    finish up final touches

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,10 @@
 "use client";
+import Image from "next/image";
 import MaxWidthWrapper from "./max-width-wrapper";
 import Link from "next/link";
+import GroqSvg from "./groq-svg";
+import { buttonVariants } from "./ui/button";
+import { Github } from "lucide-react";
 
 export const NavBar = () => {
   return (
@@ -9,6 +13,24 @@ export const NavBar = () => {
         <div className="flex items-center justify-between border-b h-14 border-zinc-200 dark:border-none ">
           <Link href="/" className="z-40 flex font-semibold">
             <span>😀plebUis😀.</span>
+          </Link>
+          <div className="flex items-center gap-2 ">
+            <h1 className="  text-slate-600">Powered by</h1>
+            <Link href={"https://groq.com/"}>
+              <Image src={"/groq.png"} height={50} width={50} alt="pleb" />
+            </Link>
+            X
+            <Link href={"https://ai.meta.com/"}>
+              <Image src={"/llama.png"} height={50} width={50} alt="pleb" />
+            </Link>
+          </div>
+          <Link
+            href={"https://github.com/mundume/quickchat-ai"}
+            className={buttonVariants({
+              size: "icon",
+            })}
+          >
+            <Github className="w-4 h-4 text-slate-600" />
           </Link>
         </div>
       </MaxWidthWrapper>
